ac63a7180436fa69629b45a40581652e6ea6b55c
jason.kok <jason.kok@pulseenergy.com>
2013-08-16 02:47:40 +0800

    Fix example.

--- munging.R
+++ munging.R
@@ -76,7 +76,7 @@ matrix <- do.call(rbind, some_list)
 
 # lookup and match
 us.states <- c("ak","al","ar","az","ca","co","ct","de","fl","ga","hi","ia","id","il", "in","ks","ky","la","ma","md","me","mi","mn","mo","ms","mt","nc","nd","ne","nh", "nj","nm","nv","ny","oh","ok","or","pa","ri","sc","sd","tn","tx","ut","va","vt", "wa","wi","wv","wy")
-data$states <- data$states[match(us.states)]
+data$states <- data$states[match(data$states, us.states)]
 
 # subset of data (just put in col name)
 # same as bracket notation
