050a6118b3a63c811ffefc461f2d73d58b0418f2
jason.kok <jason.kok@pulseenergy.com>
2013-08-21 15:49:58 +0800

    EM example.

--- clustering.R
+++ clustering.R
@@ -9,3 +9,18 @@ kmeans.ex <- function() {
     data.frame(data$col.label[o], clusters$cluster[o])
 
 }
+
+clustering.EM <- function () {
+
+    library(mixtools)
+    result <- mvnormalmixEM(data$X, arbvar=T, k=k, epsilon=1e-2)
+
+    prob <- round(result$posterior[,1])
+
+    plot(data$colx, data$coly, type="n")
+    text(x=data$colx, y=data$coly, labels=data$col.label, col=prob+1)
+
+    o <- order(prob)
+    data.frame(data$col.label, prob[o])
+
+}
